e06e9006d1b582e64b48a9e767061dd94be324fa
jack <jack@chengyou.com>
2018-05-14 20:08:48 +0800

    1

--- emacs_conf/mycscope.sh
+++ emacs_conf/mycscope.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-cd `dirname $0`
 find . -type f -name "*.[hc]pp" > cscope.files
 find . -type f -name "*.[hc]" >> cscope.files
 cscope -bq
 ~/.emacs.conf/create_tags_file.sh < cscope.files
-
