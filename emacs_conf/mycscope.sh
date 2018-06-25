cddc9994728545f13d1407c92916ce830b529b74
jack <dfdf@company.com>
2018-06-25 21:13:28 +0800

    1

--- emacs_conf/mycscope.sh
+++ emacs_conf/mycscope.sh
@@ -4,5 +4,6 @@ find . -type f -name "*.[hc]" | grep -v CMakeFiles >> cscope.files
 find . -type f -name "*.[jt]s" | grep -v CMakeFiles >> cscope.files
 find . -type f -name "*.lua" | grep -v CMakeFiles >> cscope.files
 find . -type f -name "*.php" | grep -v CMakeFiles >> cscope.files
+dos2unix `cat cscope.files`
 cscope -bq
 #~/.emacs.conf/create_tags_file.sh < cscope.files
