0ac6c1bfe5fae9f762e492da4144d1e3cfd635a1
jack <dfdf@company.com>
2018-12-27 10:47:24 +0800

    1

--- emacs_conf/my-lsp-conf.lisp
+++ emacs_conf/my-lsp-conf.lisp
@@ -18,16 +18,24 @@
 		(if (eq major-mode 'php-mode)
 		    nil
                 ;;              (require 'cquery)
-                (require 'ccls)
+                  (require 'ccls)
+
+		  ;;eglot config
+		  ;; (eglot-ensure)
+		  ;; (company-mode)
+		  ;; (setq-local company-backends (add-to-list 'company-backends 'company-capf))
+
+		  ;;lsp config
                 (lsp)
 		(flycheck-select-checker 'c/c++-clangcheck)
-;;		(flymake-mode-off)
+		(flymake-mode-off)
+		(setq-local company-backends (add-to-list 'company-backends 'company-lsp))
 		))))
-
 
 (dolist (hook '(typescript-mode-hook js-mode-hook js2-mode-hook rjsx-mode-hook))
   (add-hook hook
             #'(lambda ()
+		(define-key js-mode-map [(meta ?,)] #'xref-find-references)		
 		(define-key js-mode-map [(meta ?.)] #'xref-find-definitions)
                 (lsp))))
 
@@ -38,7 +46,7 @@
 ;;		(flymake-mode-off)
 		)))
 
-(push 'company-lsp company-backends)
+;; (push 'company-lsp company-backends)
 (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
 (setq xref-prompt-for-identifier '(not xref-find-references xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame))
 ;;(add-hook 'lsp-mode-hook 'lsp-ui-mode)
@@ -46,3 +54,21 @@
 (global-set-key (kbd "M-,") (function xref-find-references))
 
 (setq lsp-auto-guess-root nil)
+
+
+(defun projectile-project-find-function (dir)
+  (let* ((root (projectile-project-root dir)))
+    (and root (cons 'transient root))))
+
+(with-eval-after-load 'project
+  (add-to-list 'project-find-functions 'projectile-project-find-function))
+
+
+(defun flycheck-mode-on ()
+  "Turn Flymake mode on."
+  (flycheck-mode 1))
+
+;;;###autoload
+(defun flycheck-mode-off ()
+  "Turn Flymake mode off."
+  (flycheck-mode 0))
