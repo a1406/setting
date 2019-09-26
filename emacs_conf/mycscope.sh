fcab9da02f75ffa1aa42e84b84b6192cc58c3870
jack <jack@arch.com>
2019-09-26 11:42:51 +0800

    1

--- emacs_conf/mycscope.sh
+++ emacs_conf/mycscope.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 find . -type f -name "*.[hc]pp" | grep -v CMakeFiles | grep -v "ccls-cache" > cscope.files
-find . -type f -name "*.[hc]" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
+find . -type f -name "*.[hc]" | grep -v CMakeFiles | grep -v "ccls-cache" | grep -v ".*\.pb\.h" | grep -v ".*\.pb-c\.[hc]"  >> cscope.files
 find . -type f -name "*.[jt]s" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find . -type f -name "*.lua" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find . -type f -name "*.php" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
+find . -type f -name "*.proto" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 #dos2unix `cat cscope.files` >/dev/null 2>/dev/null
 cscope -bq
 ~/.emacs.conf/create_cscopefile_tags.sh
