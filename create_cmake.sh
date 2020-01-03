b435ee49c588a3ff14d06b599fbaf0f67339d3fe
jack <jack@arch.com>
2020-01-03 10:56:34 +0800

    11

--- create_cmake.sh
+++ create_cmake.sh
@@ -15,7 +15,7 @@ echo "  )"
 
 echo ""
 
-inc=`find . -type f -name "*.h"   | xargs dirname | uniq`
+inc=`find . -type f -name "*.h" | grep -v '\.ccls-cache'  | xargs dirname | uniq`
 for t in ${inc}
 do
     echo "INCLUDE_DIRECTORIES(\"\${PROJECT_SOURCE_DIR}/${t}\")"
@@ -23,6 +23,12 @@ done
 
 echo ""
 
+echo "FOREACH(src \${SRC_LIST})"
+echo "  IF(NOT (src MATCHES \".*[.]ccls-cache.*\") AND NOT(src MATCHES \".*/CMakeFiles/.*\"))"
+echo "    LIST(APPEND SRC_LIST2 \${src})"
+echo "  ENDIF()"
+echo "ENDFOREACH()"
+
 echo "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY \${CMAKE_CURRENT_SOURCE_DIR})"
-echo "add_executable  (game_srv \${SRC_LIST})"
+echo "add_executable  (game_srv \${SRC_LIST2})"
 
