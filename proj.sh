e3a2fde4c8b6b52f01fe8f2a1f14b5b17fd6aa84
jack <jack@arch.com>
2020-01-21 11:48:07 +0800

    1

--- proj.sh
+++ proj.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
+# rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
 rm -rf .ccls-cache
+rm -rf tmpbuild
 sh ~/.emacs.conf/mycscope.sh
 ~/gitroot/setting/create_cmake.sh > CMakeLists.txt
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -Btmpbuild .
+cp tmpbuild/compile_commands.json .
 ~/gitroot/setting/create_ccls.sh > .ccls
 ccls --index=.
 
