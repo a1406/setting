c9ab686960ea3e0360e2d1c3deb164d3db2763e3
jacktang <jack@jack-arch2.com>
2020-07-11 00:29:04 +0800

    1

--- docker/nginx-php/scripts/mysql.php
+++ docker/nginx-php/scripts/mysql.php
@@ -3,14 +3,13 @@ $servername = "mysql";
 $username = "root";
 $password = "111111";
 
-$pdo = new PDO("mysql:host=$servername, 'root', '111111');
 
-/* $conn = mysqli_connect($servername, $username, $password);
- * if(!$conn){
- *     die("数据库连接错误" . mysqli_connect_error());
- * }else{
- *     echo"数据库连接成功";
- * }
- *  */
+$conn = mysqli_connect($servername, $username, $password);
+if(!$conn){
+    die("数据库连接错误" . mysqli_connect_error());
+}else{
+    echo"数据库连接成功";
+}
+
 ?>
 
