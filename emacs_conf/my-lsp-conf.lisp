4362b24574051f77973a0b1e8c99dc1c0fe700dc
jacktang <jack@jack-arch2.com>
2020-08-09 11:05:55 +0800

    1

--- emacs_conf/my-lsp-conf.lisp
+++ emacs_conf/my-lsp-conf.lisp
@@ -148,15 +148,15 @@
 
 
 ;;避免太多的无用信息
-(defun lsp--imenu-create-index ()
-  "Create imenu index from document symbols."
-  (let ((symbols (lsp--get-document-symbols)))
-    ;; (if (lsp--imenu-hierarchical-p symbols)
-    ;;     (lsp--imenu-create-hierarchical-index symbols)
-      (mapcar (lambda (nested-alist)
-                (cons (car nested-alist)
-                      (mapcar #'lsp--symbol-to-imenu-elem (cdr nested-alist))))
-              (seq-group-by #'lsp--get-symbol-type (lsp--imenu-filter-symbols symbols)))))
+;; (defun lsp--imenu-create-index ()
+;;   "Create imenu index from document symbols."
+;;   (let ((symbols (lsp--get-document-symbols)))
+;;     ;; (if (lsp--imenu-hierarchical-p symbols)
+;;     ;;     (lsp--imenu-create-hierarchical-index symbols)
+;;       (mapcar (lambda (nested-alist)
+;;                 (cons (car nested-alist)
+;;                       (mapcar #'lsp--symbol-to-imenu-elem (cdr nested-alist))))
+;;               (seq-group-by #'lsp--get-symbol-type (lsp--imenu-filter-symbols symbols)))))
 ;; )
 
 ;;避免flychecker弹出checker相关信息
