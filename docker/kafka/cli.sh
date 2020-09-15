5a8cbfd7973ef41c50d6ec4c8d27d1f04dc4d2b9
jacktang <jack@jack-arch2.com>
2020-09-15 23:16:54 +0800

    111

--- docker/kafka/cli.sh
+++ docker/kafka/cli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -u root -it --rm --mount type=bind,source=/home/jack/docker-test/sharedata,target=/mnt/sharedata --network zookeeper-cluster_default bitnami/kafka:latest bash
+docker run -u root -it --rm --mount type=bind,source=/home/jack/docker-test/sharedata,target=/mnt/sharedata --network kafka_default bitnami/kafka:latest bash
