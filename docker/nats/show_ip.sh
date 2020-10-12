059b6a15c19b84d1db27469f2705b094affbeb15
tangpeilei <tangpeilei@mini1.cn>
2020-10-12 20:15:31 +0800

    add nats docker compose

---
+++ docker/nats/show_ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# all=`docker-compose -f ~/gitroot/setting/docker/kafka/docker-compose.yml ps | grep kafka_kafka | sed 's/^\(kafka_kafka[0-9_]*\).*/\1/g'`
+all=`docker-compose -f docker-compose.yml ps | grep 'nats_nats' | sed 's/^\(nats_nats[-0-9_]*\).*/\1/g'`
+for i in $all
+do
+    # echo "i = $i"
+    addr=`docker inspect $i | grep 'IPAddress": "[0-9.]+*' | sed 's/[^0-9.]//g'`
+    host=`docker inspect $i | grep "Hostname\"" | sed 's/^.*"Hostname": "\(.*\)",/\1/g'`
+    echo "$addr $host"
+done
