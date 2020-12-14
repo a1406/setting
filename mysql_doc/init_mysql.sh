0a0e9e34f019094dddac00315ae61b46f5a40b05
tangpeilei <tangpeilei@mini1.cn>
2020-12-14 11:13:12 +0800

    11

--- mysql_doc/init_mysql.sh
+++ mysql_doc/init_mysql.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
+
+dbname="sbtest"
+tbname="sbtest3"
+if [ "$1" != "" ]
+then 
+    tbname=$1
+fi
+
+if [ "$2" != "" ]
+then 
+    dbname=$1
+fi
+
 cat > 1.log << EOF
-drop table if exists \`sbtest3\`;
-CREATE TABLE \`sbtest3\` (
+drop table if exists \`${tbname}\`;
+CREATE TABLE \`${tbname}\` (
   \`id\` int unsigned NOT NULL AUTO_INCREMENT,
   \`k\` int unsigned NOT NULL DEFAULT '0',
   \`c\` char(120) NOT NULL DEFAULT '',
@@ -14,11 +27,12 @@ CREATE TABLE \`sbtest3\` (
   KEY \`sex_1\` (\`sex\`)
 ) ENGINE=InnoDB;
 EOF
-mysql -uroot -p123456 -h127.0.0.1 sbtest < 1.log
+
+mysql -uroot -p123456 -h127.0.0.1 ${dbname} < 1.log
 sleep 2
 for i in {1..10}; do
     # let "ii=${i} * 1000000"
     # echo "${i}: ${ii}"
-    python ./init_mysql.py --max=1000000 &
+    python ./init_mysql.py --max=100000 --table=${tbname} &
 done
 
