7f35715ee95876ff67e1ac89ba19ef590616c035
tangpeilei <tangpeilei@mini1.cn>
2021-04-06 10:51:41 +0800

    1

--- emacs_conf/mycscope.sh
+++ emacs_conf/mycscope.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 find -L . -type f -name "*.[hc]pp" | grep -v CMakeFiles | grep -v "ccls-cache" | grep -v "to_lua.cpp" > cscope.files
 find -L . -type f -name "*.[hc]" | grep -v CMakeFiles | grep -v "ccls-cache" | grep -v ".*\.pb\.h" | grep -v ".*\.pb-c\.[hc]"  >> cscope.files
-find -L . -type f -name "*.[cc]" | grep -v CMakeFiles | grep -v "ccls-cache" | grep -v ".*\.pb\.h" | grep -v ".*\.pb-c\.[hc]"  >> cscope.files
+find -L . -type f -name "*.cc" | grep -v CMakeFiles | grep -v "ccls-cache" | grep -v ".*\.pb\.h" | grep -v ".*\.pb-c\.[hc]"  >> cscope.files
 find -L . -type f -name "*.[jt]s" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find -L . -type f -name "*.lua" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find -L . -type f -name "*.php" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
@@ -9,7 +9,6 @@ find -L . -type f -name "*.proto" | grep -v CMakeFiles | grep -v "ccls-cache"  >
 find -L . -type f -name "*.java" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find -L . -type f -name "*.scala" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 find -L . -type f -name "*.go" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
-find -L . -type f -name "*.cc" | grep -v CMakeFiles | grep -v "ccls-cache"  >> cscope.files
 #dos2unix `cat cscope.files` >/dev/null 2>/dev/null
 #cscope -bq
 ~/.emacs.conf/create_cscopefile_tags.sh
