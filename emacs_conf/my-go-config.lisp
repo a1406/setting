b0bbc18370b26a5646686e9c12cda6b5afa14a74
tangpeilei <tangpeilei@mini1.cn>
2022-03-29 21:20:27 +0800

    1

--- emacs_conf/my-go-config.lisp
+++ emacs_conf/my-go-config.lisp
@@ -29,3 +29,19 @@
 
 (add-hook 'go-mode-hook 'my-go-mode-common-hook)
 
+;; buffer-file-name 为空会报错
+(defun my-go-plain_gopath (orig-fun &rest args)
+  ;; (message "display-buffer called with args %S" args)
+  (if buffer-file-name  
+  (let ((res (apply orig-fun args)))
+    res)))
+(advice-add 'go-plain-gopath :around #'my-go-plain_gopath)
+
+;; 没有src可以通过cscope.files来设置
+(defun my-go-guess-gopath (orig-fun &rest args)
+  (let ((res (apply orig-fun args)))
+    (if res
+	res
+      (my-cscope-guess-root-directory)
+      )))
+(advice-add 'go-guess-gopath :around #'my-go-guess-gopath)
