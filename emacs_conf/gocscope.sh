2cb709e63be9978b7eff3717b08a002fa209049f
jacktang <jack@jack-arch2.com>
2022-04-23 01:09:31 +0800

    1

--- emacs_conf/gocscope.sh
+++ emacs_conf/gocscope.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find -L . -type f -name "*.go" > cscope.files
+find -L . -type f -name "*.proto" >> cscope.files
 cp cscope.files rg.files
 sed -i 's/\.\/\(.*\)/--glob=\1/g' rg.files 
 gogtags -v 
