bd318af59a0ce64a62b635613a580b27d8bd3f8c
tangpeilei <tangpeilei@mini1.cn>
2023-09-04 12:11:34 +0800

    add markdown preview config

---
+++ emacs_conf/my-markdown-config.lisp
@@ -0,0 +1,10 @@
+;; https://stackoverflow.com/questions/36183071/how-can-i-preview-markdown-in-emacs-in-real-time/36189456?noredirect=1#comment104784050_36189456
+;; M-x httpd-start
+;; M-x impatient-mode
+;; M-x imp-set-user-filter RET markdown-html RET
+(defun markdown-html (buffer)
+    (princ (with-current-buffer buffer
+      (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://ndossougbe.github.io/strapdown/dist/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+    (current-buffer)))
+
+(setq httpd-host "0.0.0.0")
--- emacs_conf/my-web-python-config.lisp
+++ emacs_conf/my-web-python-config.lisp
@@ -1,4 +1,5 @@
 (load "~/.emacs.conf/flycheck-luacheck.el")
+(load "~/.emacs.conf/my-markdown-config.lisp")
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
