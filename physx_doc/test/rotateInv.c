93cf212f151be9c28b4f5fe23ec19f393a545192
jacktang <jack@jack-arch2.com>
2023-11-21 23:23:28 +0800

    1

--- physx_doc/test/rotateInv.c
+++ physx_doc/test/rotateInv.c
@@ -9,21 +9,38 @@ typedef struct PxVec3_
 } PxVec3;
 PxVec3 rotateInv(float x, float y, float z, float w, PxVec3 v)
 {
-        const float vx = 2.0f * v.x;
-        const float vy = 2.0f * v.y;
-        const float vz = 2.0f * v.z;
-        const float w2 = w * w - 0.5f;  // 1/2 cos(x)
-        const float dot2 = (x * vx + y * vy + z * vz);  // 旋转轴和坐标得点乘 * 2
+	const float vx   = 2.0f * v.x;
+	const float vy   = 2.0f * v.y;
+	const float vz   = 2.0f * v.z;
+	const float w2   = w * w - 0.5f;                // 1/2 cos(x)
+	const float dot2 = (x * vx + y * vy + z * vz);  // 旋转轴和坐标得点乘 * 2
 
-		PxVec3 ret;
-		ret.x = (vx * w2 - (y * vz - z * vy) * w + x * dot2);
-		ret.y = (vy * w2 - (z * vx - x * vz) * w + y * dot2);
-		ret.z = (vz * w2 - (x * vy - y * vx) * w + z * dot2);
-        return ret;
-//   v * cos(*)
-// - (x,y,z) CROSS v * cos(*)
-// + (x,y,z) * 2 * ((x, y, z) DOT v)
+	PxVec3 ret;
+	ret.x = (vx * w2 - (y * vz - z * vy) * w + x * dot2);
+	ret.y = (vy * w2 - (z * vx - x * vz) * w + y * dot2);
+	ret.z = (vz * w2 - (x * vy - y * vx) * w + z * dot2);
+	return ret;
+	//   v * cos(*)
+	// - (x,y,z) CROSS v * cos(* / 2) * 2
+	// + (x,y,z) * 2 * ((x, y, z) DOT v)
 }
+
+// PxVec3 rotateInv2(float x, float y, float z, float w, PxVec3 v)
+// {
+// 	float cos = (w * w - 0.5f) * 2.0;                // cos(x)
+// 	float coscos = cos * cos;
+// 	float sin = sqrtf(1.0 - coscos);
+// 	float sinsin = sin * sin;
+	
+// 	PxVec3 ret;
+// 	ret.x = v.x * coscos + v.y * (sin * cos + sinsin * cos) + v.z * (sinsin - sin * coscos);
+// 	ret.y = v.x * (-sin * cos) + v.y * (coscos - sinsin * sin) + v.z * (sin * cos + sinsin * cos);
+// 	ret.z = v.x * sin + v.y * (-sin * cos) + v.z * (cos * cos);
+// 	return ret;
+	
+// }
+
+
 #define PI (3.1415926)
 int main(int argc, char *argv[])
 {
@@ -44,6 +61,9 @@ int main(int argc, char *argv[])
 	printf("sqrt(3) = %f, sqrt(3) / 2 = %f\n", sqrtf(3.0), sqrtf(3.0) / 2);
 	PxVec3 v2 = rotateInv(x, y, z, w, v);
 	printf("(%f,  %f,   %f)\n", v2.x, v2.y, v2.z);
+	// PxVec3 v3 = rotateInv2(x, y, z, w, v);
+	// printf("(%f,  %f,   %f)\n", v3.x, v3.y, v3.z);	
 	return 0;
 }
-//	gcc -g -O0 -o test test.c -lm
+//	gcc -g -O0 -o test rotateInv.c -lm
+ 
